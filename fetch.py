573f7e3fdc9bc393dc91751973f4e1c47848c9d2
Biskiet <26069122g@connect.polyu.hk>
2026-09-23 17:00:27 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -2,17 +2,24 @@
 # requires-python = ">=3.10"
 # dependencies = ["requests"]
 # ///
-
 import requests
 import json
 from pathlib import Path
 
 DATA = Path("data")
 DATA.mkdir(exist_ok=True)
-OUT_FILE = DATA / "hk_rain_hourly.json"
+OUT_FILE = DATA / "weather_data.json"
 
-url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en"
-resp = requests.get(url)
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 59.91,
+    "longitude": 10.75,
+    "hourly": "temperature_2m",
+    "models": "metno_seamless",
+    "forecast_days": 3
+}
+
+resp = requests.get(url, params=params)
 resp.raise_for_status()
 data = resp.json()
 
